1a513d0e9b90befcdb8159ef015d415f9ec07eff
estanis22 <estanislaoperezpesado@gmail.com>
2023-02-27 16:28:58 -0300

    listado de movies listo

---
+++ movies.js
@@ -0,0 +1,140 @@
+let movies = [
+    {
+      id: 1,
+      title: "The Shawshank Redemption",
+      rating: 9.3,
+      awards: 7,
+      length: 142,
+      price: 12.99,
+      genre: "Drama"
+    },
+    {
+      id: 2,
+      title: "The Godfather",
+      rating: 9.2,
+      awards: 11,
+      length: 175,
+      price: 13.99,
+      genre: "Crime"
+    },
+    {
+      id: 3,
+      title: "Pulp Fiction",
+      rating: 8.9,
+      awards: 1,
+      length: 154,
+      price: 11.99,
+      genre: "Thriller"
+    },
+    {
+      id: 4,
+      title: "The Dark Knight",
+      rating: 9.0,
+      awards: 2,
+      length: 152,
+      price: 14.99,
+      genre: "Action"
+    },
+    {
+      id: 5,
+      title: "Forrest Gump",
+      rating: 8.8,
+      awards: 6,
+      length: 142,
+      price: 12.99,
+      genre: "Comedy-drama"
+    },
+    {
+      id: 6,
+      title: "Inception",
+      rating: 8.8,
+      awards: 4,
+      length: 148,
+      price: 13.99,
+      genre: "Science fiction"
+    },
+    {
+      id: 7,
+      title: "Titanic",
+      rating: 7.8,
+      awards: 11,
+      length: 194,
+      price: 15.99,
+      genre: "Romance"
+    },
+    {
+      id: 8,
+      title: "The Silence of the Lambs",
+      rating: 8.6,
+      awards: 5,
+      length: 118,
+      price: 10.99,
+      genre: "Horror"
+    },
+    {
+      id: 9,
+      title: "The Matrix",
+      rating: 8.7,
+      awards: 4,
+      length: 136,
+      price: 13.99,
+      genre: "Action"
+    },
+    {
+      id: 10,
+      title: "The Lord of the Rings: The Return of the King",
+      rating: 9.0,
+      awards: 11,
+      length: 201,
+      price: 17.99,
+      genre: "Fantasy"
+    },
+    {
+        id: 11,
+        title: "The Social Network",
+        rating: 7.7,
+        awards: 3,
+        length: 120,
+        price: 6.99,
+        genre: "Drama"
+    },
+    {
+        id: 12,
+        title: "La La Land",
+        rating: 8.0,
+        awards: 6,
+        length: 128,
+        price: 5.99,
+        genre: "Musical"
+      },
+      {
+        id: 13,
+        title: "Eternal Sunshine of the Spotless Mind",
+        rating: 8.3,
+        awards: 1,
+        length: 108,
+        price: 6.99,
+        genre: "Drama"
+      },
+      {
+        id: 14,
+        title: "Jurassic Park",
+        rating: 8.1,
+        awards: 3,
+        length: 127,
+        price: 7.99,
+        genre: "Action"
+      },
+      {
+        id: 15,
+        title: "Goodfellas",
+        rating: 8.7,
+        awards: 1,
+        length: 146,
+        price: 7.99,
+        genre: "Crime"
+      }
+      
+  ];
+  
+  module.exports = movies;
